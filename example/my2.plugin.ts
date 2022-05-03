915f9ec725ce0d710d717c0737aa7792686d38c5
alfredsgenkins <alfreds@scandipwa.com>
2022-05-04 02:02:54 +0300

    Added test static method

--- example/my2.plugin.ts
+++ example/my2.plugin.ts
@@ -6,6 +6,9 @@ export default {
     'Hello/World': {
         'member-function': {
             method2: pluginForMethod
+        },
+        'static-member': {
+            method3: () => 123
         }
     }
 }
